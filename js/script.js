86bf81d55470b442d3d5d29fd471be46cc96fb66
Pablo Escaida <pescaida@gmail.com>
2022-03-05 18:30:39 +0000

    running1

--- js/script.js
+++ js/script.js
@@ -7,5 +7,18 @@ $.ajax({
     },
     success: function(response){
         console.log(response);
+        $("#tar1").text(JSON.stringify(response));
+    }
+});
+
+$.ajax({
+    url: "https://reqres.in/api/login",
+    type: "POST",
+    data: {
+        "email": "eve.holt@reqres.in",
+        "password": "cityslicka"
+    },
+    success: function(response){
+        $("#tar2").text(JSON.stringify(response));
     }
 });
