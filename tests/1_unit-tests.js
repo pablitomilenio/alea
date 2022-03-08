466161a91376b44d9e5b66139aaac2ca08deca89
Pablo Escaida <pescaida@gmail.com>
2022-03-08 16:30:40 +0000

    test1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,2 +1,10 @@
 const chai = require('chai');
 const assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    // Test implementation goes here
+  });
+
+  // We can have more its here
+});
